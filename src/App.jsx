3d007ddbedfb1dd3870c26a6dadabb308b9d1e33
youxingzhi <xingzhi.you@shopee.com>
2024-05-29 10:31:00 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {use, Suspense} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const api = async () => {
+  const res = await fetch('https://api.chucknorris.io/jokes/random')
+  return res.json()
+}
 
+const Item = (props) => {
+  const joke = use(props.api)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div>{joke.value}</div>
+    </div>
+  )
+}
+
+function App() {
+  const __api = api()
+  return (
+    <div>
+      <div id='tips'>初始化时，自动获取一条数据内容</div>
+      <Suspense fallback={<div>loading...</div>}>
+        <Item api={__api} />
+      </Suspense>
+    </div>
   )
 }
 
